5218001394e6b507ad65c535967ae0bb57b43249
Favric <favrimz@gmail.com>
2021-06-30 15:06:28 -0300

    Fix component name and avoid nesting with early return.

--- react/components/ProductAvailability.tsx
+++ react/components/ProductAvailability.tsx
@@ -1,5 +1,5 @@
-import React, { memo } from 'react'
-import type { ProductTypes } from 'vtex.product-context'
+import React, {memo} from 'react'
+import type {ProductTypes} from 'vtex.product-context'
 
 import LowStock from './LowStock'
 import HighStock from './HighStock'
@@ -7,55 +7,55 @@ import ShowAvailable from './ShowAvailable'
 import Container from './Container'
 
 interface Props {
-  threshold: number
-  lowStockMessage?: string
-  highStockMessage?: string
-  showAvailability?: boolean
-  showAvailabilityMessage?: string
-  availableQuantity?: ProductTypes.CommercialOffer['AvailableQuantity']
+    threshold: number
+    lowStockMessage?: string
+    highStockMessage?: string
+    showAvailability?: boolean
+    showAvailabilityMessage?: string
+    availableQuantity?: ProductTypes.CommercialOffer['AvailableQuantity']
 }
 
 function ProductAvailability({
-  threshold,
-  lowStockMessage,
-  highStockMessage,
-  showAvailability,
-  showAvailabilityMessage,
-  availableQuantity,
-}: Props) {
-  if (!availableQuantity || availableQuantity < 1) {
-    return null
-  }
+                                 threshold,
+                                 lowStockMessage,
+                                 highStockMessage,
+                                 showAvailability,
+                                 showAvailabilityMessage,
+                                 availableQuantity,
+                             }: Props) {
+    if (!availableQuantity || availableQuantity < 1) {
+        return null
+    }
 
-  const isLowStock = availableQuantity < threshold
+    const isLowStock = availableQuantity < threshold
 
-  if (!showAvailability) {
+    if (showAvailability) {
+        return (
+            <Container>
+                <ShowAvailable
+                    showAvailabilityMessage={showAvailabilityMessage}
+                    availableQuantity={availableQuantity}
+                />
+            </Container>
+        )
+    }
+
     if (isLowStock && lowStockMessage) {
-      return (
-        <Container>
-          <LowStock
-            text={lowStockMessage}
-            availableQuantity={availableQuantity}
-          />
-        </Container>
-      )
+        return (
+            <Container>
+                <LowStock
+                    text={lowStockMessage}
+                    availableQuantity={availableQuantity}
+                />
+            </Container>
+        )
     }
 
     if (!isLowStock && highStockMessage) {
-      return <HighStock text={highStockMessage} />
+        return <HighStock text={highStockMessage}/>
     }
 
     return null
-  }
-
-  return (
-    <Container>
-      <ShowAvailable
-        showAvailabilityMessage={showAvailabilityMessage}
-        availableQuantity={availableQuantity}
-      />
-    </Container>
-  )
 }
 
 export default memo(ProductAvailability)
