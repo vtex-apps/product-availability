ce4725da37b3e5b059fe603d366c083681f3a904
victorhmp <victor2142@gmail.com>
2022-02-23 16:50:05 -0300

    Use `formatIOMessage` in user-input string props

--- react/components/ProductAvailabilityWrapper.tsx
+++ react/components/ProductAvailabilityWrapper.tsx
@@ -3,65 +3,11 @@ import { useProduct } from 'vtex.product-context'
 import type { ProductTypes } from 'vtex.product-context'
 import { useCssHandles } from 'vtex.css-handles'
 import type { CssHandlesTypes } from 'vtex.css-handles'
-import { defineMessages } from 'react-intl'
+import { formatIOMessage } from 'vtex.native-types'
+import { useIntl, defineMessages } from 'react-intl'
 
 import ProductAvailability from './ProductAvailability'
 import { CssHandlesProvider } from './CssHandlesContext'
-
-const messages = defineMessages({
-  title: {
-    defaultMessage: 'Product Availability',
-    id: 'admin/editor.product-availability.title',
-  },
-  description: {
-    defaultMessage: 'Component that shows the remaining available quantity',
-    id: 'admin/editor.product-availability.description',
-  },
-  thresholdTitle: {
-    defaultMessage: 'Threshold quantity',
-    id: 'admin/editor.product-availability.threshold.title',
-  },
-  thresholdDescription: {
-    defaultMessage:
-      'Minimum quantity that makes low stock message appear (if message is set)',
-    id: 'admin/editor.product-availability.threshold.description',
-  },
-  lowStockMessageTitle: {
-    defaultMessage: 'Low stock message',
-    id: 'admin/editor.product-availability.lowStockMessage.title',
-  },
-  lowStockMessageDescription: {
-    defaultMessage:
-      'String to be shown to user when stock is lower than threshold. Should have {quantity} inside the given string, to be replaced for the threshold property. Example: "Only {quantity} left!". Leave empty to not show.',
-    id: 'admin/editor.product-availability.lowStockMessage.description',
-  },
-  highStockMessageTitle: {
-    defaultMessage: 'High stock message',
-    id: 'admin/editor.product-availability.highStockMessage.title',
-  },
-  highStockMessageDescription: {
-    defaultMessage:
-      "String to be shown when stock is higher or equal than threshold. If left empty, won't show",
-    id: 'admin/editor.product-availability.highStockMessage.description',
-  },
-  showAvailabilityTitle: {
-    defaultMessage: 'Enable availability message',
-    id: 'admin/editor.product-availability.showAvailabilityMessage.title',
-  },
-  showAvailabilityDescription: {
-    defaultMessage: 'Option to show availability',
-    id: 'admin/editor.product-availability.showAvailabilityMessage.description',
-  },
-  showAvailabilityMessageTitle: {
-    defaultMessage: 'Availability message',
-    id: 'admin/editor.product-availability.showAvailabilityMessage.title',
-  },
-  showAvailabilityMessageDescription: {
-    defaultMessage:
-      'String to be shown when "Enable availability message" option is set to "stock"',
-    id: 'admin/editor.product-availability.showAvailabilityMessage.description',
-  },
-})
 
 const CONTAINER_CSS_HANDLES = ['container'] as const
 const LOW_STOCK_CSS_HANDLES = ['lowStockText', 'lowStockHighlight'] as const
@@ -94,7 +40,7 @@ interface Props {
   threshold: number
   lowStockMessage?: string
   highStockMessage?: string
-  showAvailability?: 'stock'
+  showAvailability?: 'stock' | 'disabled'
   showAvailabilityMessage?: string
   classes?: CssHandlesTypes.CustomClasses<
     typeof CONTAINER_CSS_HANDLES &
@@ -116,6 +62,22 @@ function ProductAvailabilityWrapper({
 }: Props) {
   const { handles, withModifiers } = useCssHandles(CSS_HANDLES, { classes })
   const productContextValue = useProduct()
+  const intl = useIntl()
+
+  const formattedLowStockMessage = formatIOMessage({
+    id: lowStockMessage,
+    intl,
+  })
+
+  const formattedHighStockMessage = formatIOMessage({
+    id: highStockMessage,
+    intl,
+  })
+
+  const formattedShowAvailabilityMessage = formatIOMessage({
+    id: showAvailabilityMessage,
+    intl,
+  })
 
   if (!productContextValue) {
     return null
@@ -131,50 +93,65 @@ function ProductAvailabilityWrapper({
     <CssHandlesProvider handles={handles} withModifiers={withModifiers}>
       <ProductAvailability
         threshold={threshold}
-        lowStockMessage={lowStockMessage}
-        highStockMessage={highStockMessage}
+        lowStockMessage={formattedLowStockMessage}
+        highStockMessage={formattedHighStockMessage}
         showAvailability={showAvailability}
-        showAvailabilityMessage={showAvailabilityMessage}
+        showAvailabilityMessage={formattedShowAvailabilityMessage}
         availableQuantity={availableQuantity}
       />
     </CssHandlesProvider>
   )
 }
 
-ProductAvailabilityWrapper.schema = {
-  title: messages.title.id,
-  description: messages.description.id,
-  type: 'object',
-  properties: {
-    threshold: {
-      title: messages.thresholdTitle.id,
-      description: messages.thresholdDescription.id,
-      type: 'number',
-      default: 0,
-      isLayout: true,
-    },
-    lowStockMessage: {
-      title: messages.lowStockMessageTitle.id,
-      description: messages.lowStockMessageDescription.id,
-      type: 'string',
-    },
-    highStockMessage: {
-      title: messages.highStockMessageTitle.id,
-      description: messages.highStockMessageDescription.id,
-      type: 'string',
-    },
-    showAvailability: {
-      title: messages.showAvailabilityTitle.id,
-      description: messages.showAvailabilityDescription.id,
-      type: 'enum',
-      enum: ['disabled', 'stock'],
-    },
-    showAvailabilityMessage: {
-      title: messages.showAvailabilityMessageTitle.id,
-      description: messages.showAvailabilityMessageDescription.id,
-      type: 'string',
-    },
+defineMessages({
+  title: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.title',
   },
+  lowStockMessageTitle: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.lowStockMessage.title',
+  },
+  lowStockMessageDescription: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.lowStockMessage.description',
+  },
+  highStockMessageTitle: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.highStockMessage.title',
+  },
+  highStockMessageDescription: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.highStockMessage.description',
+  },
+  showAvailabilityMessageTitle: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.showAvailabilityMessage.title',
+  },
+  showAvailabilityMessageDescription: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.showAvailabilityMessage.description',
+  },
+  thresholdTitle: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.threshold.title',
+  },
+  thresholdDescription: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.threshold.description',
+  },
+  showAvailabilityTitle: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.showAvailability.title',
+  },
+  showAvailabilityDescription: {
+    defaultMessage: 'Product Availability',
+    id: 'admin/editor.product-availability.showAvailability.description',
+  },
+})
+
+ProductAvailabilityWrapper.schema = {
+  title: 'admin/editor.product-availability.title',
 }
 
 export default ProductAvailabilityWrapper
