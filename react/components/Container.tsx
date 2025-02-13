853aee10103825db75f4cd6e6d5ad32c760565e2
AnandhanMahi <anandhan.v@trikatechnologies.com>
2025-02-13 19:56:05 +0530

    Added aria-label for product-availability

--- react/components/Container.tsx
+++ react/components/Container.tsx
@@ -6,7 +6,7 @@ import { useContextCssHandles } from './CssHandlesContext'
 function Container({ children }: PropsWithChildren<unknown>) {
   const { handles } = useContextCssHandles()
 
-  return <div className={`${handles.container} flex pv2`}>{children}</div>
+  return <div className={`${handles.container} flex pv2`} aria-label="Product Availablity">{children}</div>
 }
 
 export default Container
